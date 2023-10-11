d08d7164687ca049f1a840dbdf62e696488ac3fc
R. Mahadevan <97651598+maha201@users.noreply.github.com>
2023-10-11 06:24:55 +0530

    1360A-Minimal Square.cpp
    Added Minimal Square

---
+++ 1360A-Minimal Square.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+using namespace std;
+int main()
+{
+    int t,a,b;
+    cin>>t;
+    while(t--){
+        cin>>a>>b;
+        if(a > b){
+            swap(a,b);
+        }
+        cout<<max(a*2,b)*max(a*2,b)<<endl;
+    }
+    return 0;
+}
